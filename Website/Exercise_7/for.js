db6ca349982e67f7a62d03d99de1f5317dad69ed
msun02-art <msun02@risd.edu>
2025-10-13 17:41:02 -0400

    exercise 7

--- Website/Exercise_7/for.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-
-	const boxes = document.querySelectorAll('.box');
-	boxes.forEach((box, i) => {
-  	const color = (i % 2 === 0) ? 'red' : 'blue'; // 0,2,4... = red; 1,3,5... = blue
-  	box.style.backgroundColor = color;
-	});
-});
-
